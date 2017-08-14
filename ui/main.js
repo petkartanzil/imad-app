c777e5796aa9383058ff5ce0f695b434ea9fe6a5
petkartanzil <30769603+petkartanzil@users.noreply.github.com>
2017-08-15 00:56:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -5,6 +5,12 @@ var element=document.getElementById('main-text');
 element.innerHTML='New Value';
 
 var img=document.getElementById('madi');
+var marginLeft=0;
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.margin=marginLeft+'px';
+}
 img.onclick=function(){
+    var interval=setIntervala(moveRight, 100);
     img.style.marginleft='100px';
 };
