7abb1b98952c532c4893353d8b97ff3fa46da154
petkartanzil <30769603+petkartanzil@users.noreply.github.com>
2017-08-14 23:07:17 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,76 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var articleOne={
+    title: 'Article One | Tanzil Petkar',
+    heading:'Article One',
+    date: 'August 14, 2017',
+    content:`
+    <p>
+        This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Artic
+    <p>
+        This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.
+    </p>
+    <p>
+        This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.
+    </p>
+    <p>
+        This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.This is First Article.
+    </p>`
+    
+};
+
+function createTemplate (data){
+        var title=data.title;
+        var heading=data.heading;
+        var date=data.date;
+        var content=data.content;
+        
+        var htmlTamplate=`
+        <html>
+            <head>
+                <title>
+                  ${title}
+                </title>
+                 <link href="/ui/style.css" rel="stylesheet" />
+                
+                <meta name="viemport" content="width=device-width, initial-sacale=1" />
+            </head>
+            
+            <body>
+                <div class="container">
+                    <div>
+                        <a href="/">Home</a>
+                    </div>
+                    <hr/>
+                    
+                    <h3>
+                       ${heading}
+                    </h3>
+                    <div>
+                       ${date}
+                    </div>
+                    
+                    <div>
+                       ${content}
+                    </div>
+                </div>
+            </body>
+            
+            
+        </html>
+        `;
+        return htmlTemplate;
+}
+
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/article-one', function (req, res) {
-    res.sendFile(path.join(__dirname, 'ui', 'article-one.html'));
+    res.send(createTemplate(articleOne));
 });
 
 app.get('/article-two', function (req, res) {
